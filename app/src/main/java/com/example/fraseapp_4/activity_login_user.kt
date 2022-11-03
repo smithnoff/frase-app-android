af867ea2795e968f05655f8ca68549998aa872a6
juan0acu <juan0acu@gmail.com>
2022-11-03 00:12:17 -0300

    Nueva rama

---
+++ app/src/main/java/com/example/fraseapp_4/activity_login_user.kt
@@ -0,0 +1,11 @@
+package com.example.fraseapp_4
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class activity_login_user : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login_user)
+    }
+}
