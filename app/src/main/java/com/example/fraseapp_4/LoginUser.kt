6d0efe5ad5bbbaef7c117a8c0099b24873032924
juan0acu <juan0acu@gmail.com>
2022-11-03 01:21:04 -0300

    Avance nuevo login

--- app/src/main/java/com/example/fraseapp_4/LoginUser.kt
+++ app/src/main/java/com/example/fraseapp_4/LoginUser.kt
@@ -3,7 +3,7 @@ package com.example.fraseapp_4
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class activity_login_user : AppCompatActivity() {
+class LoginUser : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login_user)
--- app/src/main/java/com/example/fraseapp_4/SplashScreen.kt
+++ app/src/main/java/com/example/fraseapp_4/SplashScreen.kt
@@ -10,7 +10,7 @@ class SplashScreen : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
         Handler().postDelayed({
-                              startActivity(Intent(this,LoginActivity:: class.java))
+                              startActivity(Intent(this,LoginUser:: class.java))
             finish()
         },3000L)
     }
